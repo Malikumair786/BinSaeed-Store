091b9343636135f87c87d2c1484982b561690958
Umair Malik <mu5631160@gmail.com>
2025-01-18 23:51:10 +0500

    Progressive web app configurations completed

---
+++ user-app/next.config.mjs
@@ -0,0 +1,22 @@
+/** @type {import('next').NextConfig} */
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  disable: false,
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+});
+
+export default withPWA({
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+});
--- user-app/src/app/(loggedIn)/dashboard/page.tsx
+++ user-app/src/app/(loggedIn)/dashboard/page.tsx
@@ -4,8 +4,51 @@ import Cookies from "js-cookie";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
 
+import { useEffect, useState } from "react";
+interface BeforeInstallPromptEvent extends Event {
+  prompt: () => Promise<void>;
+  userChoice: Promise<{ outcome: "accepted" | "dismissed"; platform: string }>;
+}
 const Dashbooard = () => {
   const router = useRouter();
+
+  const [deferredPrompt, setDeferredPrompt] =
+    useState<BeforeInstallPromptEvent | null>(null);
+  const [promptDismissed, setPromptDismissed] = useState(false);
+
+  useEffect(() => {
+    const handleBeforeInstallPrompt = (e: Event) => {
+      e.preventDefault();
+      setDeferredPrompt(e as BeforeInstallPromptEvent);
+    };
+
+    const handleUserClick = () => {
+      if (deferredPrompt && !promptDismissed) {
+        deferredPrompt.prompt();
+        deferredPrompt.userChoice.then((choiceResult: { outcome: string }) => {
+          if (choiceResult.outcome === "accepted") {
+            console.log("App installed");
+          } else {
+            console.log("App not installed");
+            setPromptDismissed(true);
+          }
+          setDeferredPrompt(null);
+        });
+      }
+    };
+
+    window.addEventListener("beforeinstallprompt", handleBeforeInstallPrompt);
+
+    window.addEventListener("click", handleUserClick);
+
+    return () => {
+      window.removeEventListener(
+        "beforeinstallprompt",
+        handleBeforeInstallPrompt
+      );
+      window.removeEventListener("click", handleUserClick);
+    };
+  }, [deferredPrompt, promptDismissed]);
 
   const handleLogout = () => {
     Cookies.remove("access_token");
